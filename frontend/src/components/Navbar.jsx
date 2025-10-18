38530c61871ab7de2efd888571cfad83a2bc87e1
rupampaul123 <sunyrony1234@gmail.com>
2025-10-18 15:49:35 +0530

    update3

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar()
     const[role, setRole] = useState([]);
 
     useEffect(() => {
-    fetch("https://venuex-production.up.railway.app//me", {
+    fetch("https://venuex-production.up.railway.app/me", {
       method: "GET",
       credentials: "include", 
     })
