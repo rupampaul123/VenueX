e5fbc9c75b03e62a4d00645cf59171e6e632a1de
rupampaul123 <sunyrony1234@gmail.com>
2025-10-18 15:33:19 +0530

    db deployed

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use(cors({
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
-mongoose.connect("mongodb://127.0.0.1:27017/VenueX")
+mongoose.connect("mongodb+srv://rupampaulbag:ronsunbum@cluster0.n2kiclx.mongodb.net/VenueX")
   .then(() => console.log("database connected"))
   .catch((err) => console.log(err));
 
